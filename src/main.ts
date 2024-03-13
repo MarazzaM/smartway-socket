ab9ffc4b022aa7989a562e19e5b7adf424af9bc8
MarazzaM <marazza.matias@gmail.com>
2024-03-13 11:35:59 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('My backend')
+    .setDescription('The backend description')
+    .setVersion('0.1')
+    .addTag('fullstack')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
