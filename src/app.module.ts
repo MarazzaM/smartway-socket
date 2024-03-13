8bb4ec5ed0aa98ed4b53be8b5b51d8902393d495
MarazzaM <marazza.matias@gmail.com>
2024-03-13 12:02:06 -0300

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QueueGateway } from './queue/queue.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3310,
+      username: 'novosga_us',
+      password: '123456',
+      database: 'novosga_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, QueueGateway],
 })
